c03bb3a32ba28af6b96bc420dc6bb305e7eb103f
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-09-28 16:32:37 +0000

    fix: Add site and base to mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://open-source-uc.github.io',
+  base: 'staff',
   integrations: [tailwind()]
 });
